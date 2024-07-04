b4f1bd988c109120bb52735beea7207171e468cf
Sascha <sascha@localhost.localdomain>
2024-07-04 10:23:15 +0200

    Initial commit

---
+++ src/components/SpreadshopComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- src="https://shop.spreadshirt.com/ID/" -->
+    <iframe
+      src="https://fuzzl-streetwear.myspreadshop.net/" 
+      frameborder="0"
+      width="100%"
+      height="600"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SpreadshopComponent'
+}
+</script>
+
+<style scoped>
+iframe {
+  border: none;
+}
+</style>
