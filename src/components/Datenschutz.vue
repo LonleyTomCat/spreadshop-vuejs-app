e0fdae91a0b45b2232427d521a492f725f8b1900
Sascha <sascha@localhost.localdomain>
2024-07-09 11:15:58 +0200

    Set up proxy for Spreadshop iframe and add http-proxy-middleware

---
+++ src/components/Datenschutz.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Datenschutz</h1>
+    <router-link to="/">zurück zum Shop</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Datenschutz'
+};
+</script>
+
