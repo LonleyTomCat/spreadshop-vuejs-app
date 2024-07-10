05adbc4282f42ce7ae6e066c39af583865c6ba09
LonleyTomCat <91789424+LonleyTomCat@users.noreply.github.com>
2024-07-10 18:01:05 +0000

    Update SpreadshopComponent.vue

--- src/components/SpreadshopComponent.vue
+++ src/components/SpreadshopComponent.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="myShop">
-        <a href="https://fussel-streetwear.myspreadshop.de">fussel-streetwear</a>
+        <div  class="fussle-loader">
+            <span><img sizes="300x300" src="https://fussel-streetwear.myspreadshop.de/1325496/shopData/images/logo/safari-pinned-tab_qa3uf2.svg"><h1>™</h1></span>
+        </div>
     </div>
 </template>
 
@@ -28,4 +30,6 @@ export default {
 
 <style scoped>
 /* Add scoped styles if needed */
+.fussle-loader{width:100%; text-align:center;}
+h1 {position:absolute; font-size: 3em; margin-top: -450px; margin-left:65%;}
 </style>
