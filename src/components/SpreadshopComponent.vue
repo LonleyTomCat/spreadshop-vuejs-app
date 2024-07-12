548a82b37820a2f07424fb164aee4dcd9e7ce19d
LonleyTomCat <91789424+LonleyTomCat@users.noreply.github.com>
2024-07-12 12:45:28 +0000

    Update SpreadshopComponent.vue

--- src/components/SpreadshopComponent.vue
+++ src/components/SpreadshopComponent.vue
@@ -30,6 +30,6 @@ export default {
 
 <style scoped>
 /* Add scoped styles if needed */
-.fussle-loader{width:100%; text-align:center;}
-h1 {position:absolute; font-size: 3em; margin-top: -450px; margin-left:65%;}
+.fussle-loader span img {width:100%; text-align:center;}
+.fussle-loader span h1 {position:absolute; font-size: 3em; margin-top: -450px; margin-left:65%;}
 </style>
